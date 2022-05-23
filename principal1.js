620e4aa24dced206ebc222dd99111bfba9dc9501
RMulato07 <102757617+RMulato07@users.noreply.github.com>
2022-05-23 16:42:51 -0300

    Add files via upload

---
+++ principal1.js
@@ -0,0 +1,84 @@
+//               paulo
+var paciente = document.querySelector('#primeiro-paciente');
+var valorPeso = paciente.querySelector('.info-peso');
+var peso = parseInt (valorPeso.textContent);
+
+var valorAltura = paciente.querySelector('.info-altura');
+var altura = parseInt(valorAltura.textContent);
+
+var valorGordura = paciente.querySelector('.info-gordura');
+var gordura = parseInt (valorGordura.textContent);
+
+var valorIMC = parseInt (peso + altura + gordura) / 3
+console.log (valorIMC)
+var subIMC = paciente.querySelector('.info-imc')
+subIMC.textContent = valorIMC
+
+//               joao
+var paciente = document.querySelector('#segundo-paciente');
+var valorPeso = paciente.querySelector('.info-peso');
+var peso = parseInt (valorPeso.textContent); 
+
+var valorAltura = paciente.querySelector('.info-altura');
+var altura = parseInt(valorAltura.textContent);
+
+var valorGordura = paciente.querySelector('.info-gordura');
+var gordura = parseInt (valorGordura.textContent);
+
+var valorIMC = parseInt (peso + altura + gordura) / 3
+console.log (valorIMC)
+var subIMC = paciente.querySelector('.info-imc')
+subIMC.textContent = valorIMC
+
+//             erica
+
+var paciente = document.querySelector('#terceiro-paciente');
+var valorPeso = paciente.querySelector('.info-peso');
+var peso = parseInt (valorPeso.textContent); 
+
+var valorAltura = paciente.querySelector('.info-altura');
+var altura = parseInt(valorAltura.textContent);
+
+var valorGordura = paciente.querySelector('.info-gordura');
+var gordura = parseInt (valorGordura.textContent);
+
+var valorIMC = parseInt (peso + altura + gordura) / 3
+console.log (valorIMC)
+var subIMC = paciente.querySelector('.info-imc')
+subIMC.textContent = valorIMC
+
+//               Douglas
+
+var paciente = document.querySelector('#quarto-paciente');
+var valorPeso = paciente.querySelector('.info-peso');
+var peso = parseInt (valorPeso.textContent); 
+
+var valorAltura = paciente.querySelector('.info-altura');
+var altura = parseInt(valorAltura.textContent);
+
+var valorGordura = paciente.querySelector('.info-gordura');
+var gordura = parseInt (valorGordura.textContent);
+
+var valorIMC = parseInt (peso + altura + gordura) / 3
+console.log (valorIMC)
+
+var subIMC = paciente.querySelector('.info-imc')
+subIMC.textContent = valorIMC
+
+//                  Tatiana
+
+var paciente = document.querySelector('#quinto-paciente');
+var valorPeso = paciente.querySelector('.info-peso');
+var peso = parseInt (valorPeso.textContent); 
+
+var valorAltura = paciente.querySelector('.info-altura');
+var altura = parseInt(valorAltura.textContent);
+
+var valorGordura = paciente.querySelector('.info-gordura');
+var gordura = parseInt (valorGordura.textContent);
+
+var valorIMC = parseInt (peso + altura + gordura) / 3
+console.log (valorIMC)
+
+var subIMC = paciente.querySelector('.info-imc')
+subIMC.textContent = valorIMC
